87168770dee76a54787efffcd766c0c3ccf13b8c
matteus.vargas <vargas.simao@gmail.com>
2020-06-14 18:38:14 -0300

    Mesclando com o monitor de recursos

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import extra_lib.metamodel as mmodel
 import matplotlib.pyplot as plt
-import numpy
 import random
 import tools.monitor as monitor
 
@@ -33,13 +32,14 @@ if (segue == 1):
 # Instanciação das variáveis globais principais
 best = -100000
 high_bound = 12  # Limite superior, útil para estimar as gerações
-populations = ([[random.randint(1, 12) for x in range(5)] for i in range(4)])  # População inicial
+populations = ([[random.randint(1, 12) for x in range(5)] for i in range(4)])
 parents = []  # Pais nos crossovers
 melhores_scores = []  # Eleição dos melhores fitness
 melhores_cromossomos = []  # Eleição dos melhores cromossomos
 crossover_results = []  # Resultado dos crossovers
 scores = []
 geracoes = []
+melhores = []
 mem = []
 cpu = []
 pid = []
@@ -57,120 +57,68 @@ def generations():
     return high_bound ** 3  # Estima uma quantidade suficientemente boa para a quantidade de gerações
 
 
-def fitness_score():  # Aqui são eleitos os cromossomos com os melhores desempenhos de sua geração
-    global populations, best, melhores_scores, melhores_cromossomos, scores, geracoes
+def fitness_score():
+    global populations, best
     fit_value = []
-    print()
+    fit_score = []
     for i in range(len(populations)):
-        fit_value.append(
-            test.predict(populations[i]))  # Aqui é aplicada a predição com cada um dos cromossomos da população.
-        # Seu resultado é guardado em uma lista
-
-    print(f'Valores de fitness: {fit_value}')
-    print()
-    fit_value, populations = zip(*sorted(zip(fit_value, populations),
-                                         reverse=True))  # Associa-se os melhores cromossomos aos seus scores, ordenado pelo score
-    best = fit_value[0]  # Variável que guarda o melhor desempenho da geração
-    print(
-        f'População ordenada por score: {populations}')  # População ordenada pelo score, do melhor ao pior. Esse esquema será base para o crossover
-    scores.append(best)
-    melhores_scores.append(best)  # Guardam os scores ordenados
-    melhores_cromossomos.append(populations[0])  # Guarda o melhor cromossomo da geração
+        fit_value.append(test.predict(populations[i]))
+    fit_value, populations = zip(*sorted(zip(fit_value, populations), reverse=True))
+    best = fit_value[0]
+    melhores.append(best)
 
 
-def selectparent():  # Escolhem-se os pais
-    global parents, populations
-    parents.clear()
-    # Nesse esquema, por ser uma população para, então fecham-se casais
-    # O crossover se dá pelo cruzamento do melhor com o pior, do segundo melhor com o segundo pior
-
-    parents.append(populations[0])
-    parents.append(populations[1])
-    parents.append(populations[2])
-    parents.append(populations[3])
-    print(f'Casais formados {parents}')
-    print()
+def selectparent():
+    global parents
+    # global populations , parents
+    parents = populations[0:2]
 
 
 def crossover():
-    global parents, crossover_results, populations
-    cross_point = 2  # Ponto de corte no cromossomo
-    crossover_results.clear()
-    # Para formar novos filhos, pega-se a primeira metade da mãe e junta com a segunda metade do pai
-    # O segundo filho, pegam as metades inversas, até serem formados 4 novos filhos, que
-    # Irão substituir por completo a geração anterior, sem elitismo
-    crossover_results.append(parents[0][0:cross_point + 1] + parents[1][cross_point + 1:])
-    crossover_results.append(parents[0][cross_point + 1:] + parents[1][0:cross_point + 1])
-    crossover_results.append(parents[2][0:cross_point + 1] + parents[3][cross_point + 1:])
-    crossover_results.append(parents[2][cross_point + 1:] + parents[3][0:cross_point + 1])
-
-    print()
-    print(f'Crossover feito: {crossover_results}')
+    global parents
+    cross_point = random.randint(0, 3)
+    parents = parents + tuple([(parents[0][0:cross_point + 1] + parents[1][cross_point + 1:6])])
+    parents = parents + tuple([(parents[1][0:cross_point + 1] + parents[0][cross_point + 1:6])])
 
 
 def mutation():
-    global populations, crossover_results
-    # O esquema de mutação funciona como um ajuste do crossover
-    # Apelidado aqui de "correção genética", a mutação anda de gene em gene verificando se
-    # O limite superior está correto. Caso não esteja, um novo valor, dentro do seu respectivo
-    # limite é sorteado, permitindo que a regra dos limites seja mantida
-    mute = random.randint(0, 20)
-    if mute == 2:
-        for i in range(4):
-            for x in range(5):
-                if (x == 0 and (crossover_results[i][x] not in range(1, 3))):
-                    crossover_results[i][x] = random.randint(2, 3)
+    global populations, parents
+    mute = random.randint(0, 49)
+    if mute == 25:
+        x = random.randint(0, 3)
+        y = random.randint(0, 4)
 
-                if (x == 1 and (crossover_results[i][x] not in range(1, 4))):
-                    crossover_results[i][x] = random.randint(3, 4)
-
-                if (x == 2 and (crossover_results[i][x] not in range(1, 5))):
-                    crossover_results[i][x] = random.randint(4, 5)
-
-                if (x == 3 and (crossover_results[i][x] not in range(1, 6))):
-                    crossover_results[i][x] = random.randint(5, 6)
-
-                if (x == 4 and (crossover_results[i][x] not in range(1, 12))):
-                    crossover_results[i][x] = random.randint(6, 12)
-
-    populations = crossover_results
-    print()
-    print(f'População pós-mutação: {populations}')
-
-
-def ajusta_populacao():
-    global populations
-    for i in range(4):
-        for x in range(5):
-            if (x == 0):
-                populations[i][x] = random.randint(1, 3)
-            if (x == 1):
-                populations[i][x] = random.randint(2, 4)
-            if (x == 2):
-                populations[i][x] = random.randint(3, 5)
-            if (x == 3):
-                populations[i][x] = random.randint(4, 6)
-            if (x == 4):
-                populations[i][x] = random.randint(10, 12)
+        if (y == 0):
+            parents[x][y] = random.randint(1, 3)  # 1 - parents[x][y]
+        if (y == 1):
+            parents[x][y] = random.randint(1, 4)  # 1 - parents[x][y]
+        if (y == 2):
+            parents[x][y] = random.randint(1, 5)  # 1 - parents[x][y]
+        if (y == 3):
+            parents[x][y] = random.randint(1, 6)  # 1 - parents[x][y]
+        if (y == 4):
+            parents[x][y] = random.randint(1, 12)  # 1 - parents[x][y]
+    populations = parents
 
 
 def plotar_grafico(geracoes, scores):
-    global mem, cpu, pid
+    global mem, cpu
     aux = []
-    for i in range(len(scores)):
-        aux.append(float(scores[i]))
+    for i in range(len(melhores)):
+        aux.append(float(melhores[i]))
 
     plt.xlabel('Gerações')
     plt.ylabel('Fitness')
     plt.plot(geracoes, aux)
     plt.show()
-    print()
-    print("Monitoramos o uso de memória e cpu ao longo das gerações, gostaria de ver os gráficos de desempenho?")
+    print("Monitoramos o uso de memória e cpu durante as gerações. "
+          "Gostaria de ver os resultos em gráfico?")
     print()
     print("1 - Sim")
-    print("0 - Não")
+    print("0 - Nao")
     print()
     segue = int(input())
+
     if (segue == 1):
         plt.xlabel('Gerações')
         plt.ylabel('Memória')
@@ -183,10 +131,25 @@ def plotar_grafico(geracoes, scores):
         plt.show()
 
 
+def ajusta_populacao(populations):
+    for i in range(4):
+        for x in range(5):
+            if (x == 0 and (populations[i][x] not in range(1, 3))):
+                populations[i][x] = random.randint(1, 3)
+            if (x == 1 and (populations[i][x] not in range(1, 4))):
+                populations[i][x] = random.randint(2, 4)
+            if (x == 2 and (populations[i][x] not in range(1, 5))):
+                populations[i][x] = random.randint(3, 5)
+            if (x == 3 and (populations[i][x] not in range(1, 6))):
+                populations[i][x] = random.randint(4, 6)
+            if (x == 4 and (populations[i][x] not in range(1, 12))):
+                populations[i][x] = random.randint(10, 12)
+
+
+ajusta_populacao(populations)
+print(f'População inicial {populations}')
 if (segue == 1):
     M = generations()
-    ajusta_populacao()  # O ajuste aqui serve para corrigir falhas que possa ter ocorrido na criação
-    # da população inicial, em função dos limites superiores de cada variável
     for i in range(M):
         geracoes.append(i)
         print()
@@ -194,8 +157,6 @@ if (segue == 1):
         print(
             f'-------------------------------------- Iniciando o AG, geração {i} --------------------------------------')
         print()
-        print()
-        print(f'População inicial da geração {i}: {populations}')
         print()
         print('-------------------------------------- Aplicando o fitness --------------------------------------')
         fitness_score()
@@ -213,9 +174,9 @@ if (segue == 1):
 
     print()
     # Informa para o usuário os resultados
-    melhores_scores, melhores_cromossomos = zip(*sorted(zip(melhores_scores, melhores_cromossomos), reverse=True))
-    plotar_grafico(geracoes, scores)
-    print(f'Melhor fitness {melhores_scores[0]}')
+    print(f"Melhor resultado : {best}")
     print()
-    print(f'Melhor cromossomo {melhores_cromossomos[0]}')
+    print(f"Melhor cromossomos: {populations[0]}")
     print()
+
+    plotar_grafico(geracoes, melhores)
